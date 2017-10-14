a3eda6e2d6d25782ee8ba5f9db985de3dfd763be
Vlad Chernychko <consul99@ukr.net>
2017-10-14 03:09:40 +0300

    Refactored code with Rubocop

--- Homework 1/names.rb
+++ Homework 1/names.rb
@@ -1,29 +1,25 @@
 names = [
-  "Stuart Blanda", "Kaden Batz", "Cornell Durgan", "Mrs. Kieran Goldner",
-  "Miss Emile Littel", "Carolyne Terry", "Mrs. Ronaldo Buckridge",
-  "Cristal Braun", "Dr. Joanne Bartoletti", "Kenton Block", "Jeromy Medhurst",
-  "Gussie Daniel V", "Kitty Senger Jr.", "Lexus Gibson", "Kaylah Howell",
-  "Maxie Wilderman", "Kayden Mohr", "Devonte Skiles", "Tina Mante", "Heloise Mann"
+  'Stuart Blanda', 'Kaden Batz', 'Cornell Durgan', 'Mrs. Kieran Goldner',
+  'Miss Emile Littel', 'Carolyne Terry', 'Mrs. Ronaldo Buckridge',
+  'Cristal Braun', 'Dr. Joanne Bartoletti', 'Kenton Block', 'Jeromy Medhurst',
+  'Gussie Daniel V', 'Kitty Senger Jr.', 'Lexus Gibson', 'Kaylah Howell',
+  'Maxie Wilderman', 'Kayden Mohr', 'Devonte Skiles', 'Tina Mante',
+  'Heloise Mann'
 ]
+
+PREFIXES = %w[Dr. Mrs. Miss Jr. V].freeze
+
+names_without_prefixes = names.map { |x| x.split - PREFIXES }
 
 # 1
 ## Generate a hash.
 ### key will be a first name (except Miss, Dr.)
 ### value will be a second name
 
-task1Hash={}
-names.each do |element|
-	x=element.split
-	x.delete_if{ |y| y=="Dr." or y=="Mrs." or y=="Miss"}
-	if x.size == 3
-		task1Hash[x[0]]="#{x[1]} #{x[2]}"
-	else
-		task1Hash[x[0]]=x[1]
-	end
-end
-puts "First names task results"
+task_1_hash = names_without_prefixes.to_h
+puts 'First names task results'
 puts ''
-puts task1Hash
+puts task_1_hash
 puts ''
 
 # 2
@@ -31,35 +27,26 @@ puts ''
 ### key will be a first letter of a first name
 ### value will be a second letter of a second name
 
-task2Hash = {}
-names.each do |element|
-	x=element.split
-	x.delete_if{ |y| y=="Dr." or y=="Mrs." or y=="Miss"}
-	task2Hash[x[0][0]] = x[1][1]
-end
-puts "Second names task results"
+task_2_hash = names_without_prefixes.map { |x| x[0][0].split + x[1][1].split }
+task_2_hash = task_2_hash.to_h
+puts 'Second names task results'
 puts ''
-puts task2Hash
+puts task_2_hash
 puts ''
 
 # 3
 ## Generate an array.
 ### elements will be all a uniq letters of first name and second name
 #### need to sort it in alphabetical order
-#### Example: ["Stuart Blanda", "Kaden Batz", "Cornell Durgan", "Mrs. Kieran Goldner"]
+#### Example: ["Stuart Blanda", "Kaden Batz", "Cornell Durgan",
+#### "Mrs. Kieran Goldner"]
 ##### will be ['S', 'B', 'K', 'C', 'D', 'G']
 
-task3Array = []
-names.each do |element|
-	x=element.split
-	x.delete_if{ |y| y=="Dr." or y=="Mrs." or y=="Miss"}
-	task3Array.push(x[0][0],x[1][0])
-end
-task3Array.sort!.uniq!
-
-puts "Third names results"
+task_3_array = names_without_prefixes.map { |x| x[0][0].split + x[1][0].split }
+task_3_array.flatten!.sort!.uniq!
+puts 'Third names task results'
 puts ''
-print task3Array
+print task_3_array
 puts ''
 
 # 4
@@ -67,20 +54,10 @@ puts ''
 ### elements will stay in same position
 ### need to replace first name with second name
 
-task4Array = []
-names.each do |element|
-	x=element.split
-  if x.include?("Dr.") or x.include?("Mrs.") or x.include?("Miss")
-		task4Array.push("#{x[0]} #{x[2]} #{x[1]}")
-	elsif x.size==3
-		task4Array.push("#{x[1]} #{x[0]} #{x[2]}")
-	else
-		task4Array.push("#{x[1]} #{x[0]}")
-	end
-end
+task_4_array = names_without_prefixes.map { |x| x.reverse.join(' ') }
 
 puts ''
-puts "Fourth names results"
+puts 'Fourth names task results'
 puts ''
-print task4Array
+print task_4_array
 puts ''
--- Homework 1/numbers.rb
+++ Homework 1/numbers.rb
@@ -1,9 +1,9 @@
-numbers = [
-  "9461055376", "1098029425", "1365826266", "3651652057",
-  "6639633927", "9198606875", "3425027805", "9600912575",
-  "8826860435", "4244213108", "8930423446", "6975487430",
-  "2956968315", "4024720801", "8882048673", "4897211695",
-  "6089600553", "4272319905", "4517433227", "5565635133"
+numbers = %w[
+  9461055376 1098029425 1365826266 3651652057
+  6639633927 9198606875 3425027805 9600912575
+  8826860435 4244213108 8930423446 6975487430
+  2956968315 4024720801 8882048673 4897211695
+  6089600553 4272319905 4517433227 5565635133
 ]
 
 # 1
@@ -11,17 +11,14 @@ numbers = [
 ### key will be an element itself
 ### value will be a max digit in a element
 
-task1Hash = {}
+task_1_hash = {}
 numbers.each do |x|
-  task1Hash[x] = x.split(//).max
+  task_1_hash[x] = x.split(//).max
 end
-puts "First numbers task results"
+puts 'First numbers task results'
 puts ''
-puts task1Hash
+puts task_1_hash
 puts ''
-
-
-
 
 # 2
 ## Generate a string
@@ -29,26 +26,26 @@ puts ''
 ### Example: numbers = [ "9461055376", "4517433227" ]
 ### will be "9071"
 
-task2String = ""
+task_2_string = ''
 numbers.each do |x|
-  task2String = "#{task2String}#{x.split(//).max}#{x.split(//).min}"
+  task_2_string = "#{task_2_string}#{x.split(//).max}#{x.split(//).min}"
 end
-puts "Second numbers task results"
+puts 'Second numbers task results'
 puts ''
-puts task2String
+puts task_2_string
 puts ''
 
 # 3
 ## Generate a number
 ### number will be a sum of two maximum and two minimum elements
 
-t3Arr = numbers
+task_3_array = numbers
 
-t3Arr.sort!
-task3Number = t3Arr[0].to_i + t3Arr[1].to_i + t3Arr[-1].to_i + t3Arr[-2].to_i
+task_3_array.sort!
+task_3_number = task_3_array[0].to_i + task_3_array[1].to_i +
+                + task_3_array[-1].to_i + task_3_array[-2].to_i
 
-puts "Third numbers task results"
+puts 'Third numbers task results'
 puts ''
-puts task3Number
+puts task_3_number
 puts ''
-
